dc9157ac0485b7acd85531d8ba3452aa64c245c2
Thomas Moraes <tho.vini7@gmail.com>
2023-12-06 21:59:49 -0300

    Alterando permissoes e cacheando rotas

--- config/routes.php
+++ config/routes.php
@@ -6,6 +6,9 @@ use App\Presentation\Controllers\UserController;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);;
 
+$routeCollector = $app->getRouteCollector();
+$routeCollector->setCacheFile(__DIR__ . "/../storage/cache/cache.file");
+
 $app->post("/users", [UserController::class, 'store']);
 $app->get("/users/{uuid}",[UserController::class, 'show']);
 $app->delete("/users/{uuid}",[UserController::class, 'destroy']);
