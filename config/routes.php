228ff66dd60d18ed8a36f6cc8f5fc71f3bb76ac5
Thomas Moraes <tho.vini7@gmail.com>
2023-12-07 21:11:11 -0300

    Instalando o Template Engine Twig

--- config/routes.php
+++ config/routes.php
@@ -2,14 +2,24 @@
 
 use Slim\Factory\AppFactory;
 use App\Presentation\Controllers\UserController;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use Slim\Views\Twig;
 
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);;
+$app->addErrorMiddleware(true, true, true);
 
-$routeCollector = $app->getRouteCollector();
-$routeCollector->setCacheFile(__DIR__ . "/../storage/cache/cache.file");
+// $routeCollector = $app->getRouteCollector();
+// $routeCollector->setCacheFile(__DIR__ . "/../storage/cache/cache.file");
 
 $app->post("/users", [UserController::class, 'store']);
 $app->get("/users/{uuid}",[UserController::class, 'show']);
 $app->delete("/users/{uuid}",[UserController::class, 'destroy']);
 $app->put("/users/{uuid}",[UserController::class, 'update']);
+
+$app->get("/", function (Request $request, Response $response, array $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'profile.html', [
+        'name' => 'Slim Framework 4',
+    ]);
+});
--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
 
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
 require_once __DIR__ . "/../config/bootstrap.php";
 
 require_once __DIR__ . "/../config/routes.php";
+
+$twig = Twig::create(__DIR__ . "/../resources/views", ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->run();
